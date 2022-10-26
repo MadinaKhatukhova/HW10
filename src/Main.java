a88d8d43343a917369c704743c0628b1fbe5b0ff
Madina <madina-khat@yandex.ru>
2022-10-26 15:20:02 +0300

    Задача 4

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,10 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        double number = 0;
-        int[] arr = new int[1]; {
-        System.out.printf("Сумма трат за месяц составила %.0f рублей.", number);
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int c = reverseFullName.length - 1; c >= 0; c--) {
+            System.out.print(reverseFullName[c] + " ");
 
-            Arrays.sort(arr);
-            System.out.printf("Минимальная сумма трат за день составила %d рублей.", arr[0]);
-            System.out.printf("Максимальная сумма трат за день составила %d рублей.", arr[arr.length - 1]);
+        }
 
-            //Задача 3
-
-            double number1 = 199;
-            double averageSum = number1 / arr.length - 1;
-            System.out.printf("Средняя сумма трат за месяц составила %.2f рублей", averageSum);
-    }}}
-
-
-
+    }}
