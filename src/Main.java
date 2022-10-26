09af2714336a2ec7f0e585a464fbb0747978fc0a
Madina <madina-khat@yandex.ru>
2022-10-26 14:54:03 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         double number = 0;
         int[] arr = new int[1]; {
         System.out.printf("Сумма трат за месяц составила %.0f рублей.", number);
 
-        }
-        }}
+            Arrays.sort(arr);
+            System.out.printf("Минимальная сумма трат за день составила %d рублей.", arr[0]);
+            System.out.printf("Максимальная сумма трат за день составила %d рублей.", arr[arr.length - 1]);
+
+    }}}
+
 
 
